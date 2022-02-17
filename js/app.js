fcd65d751566b1c22f4caba88f361b69bf7d4268
srilokanas <67470659+srilokanas@users.noreply.github.com>
2022-02-17 10:18:21 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var x = document.getElementById("navbar-link");
+  if (x.className === "nav-center") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-center";
+  }
+}
